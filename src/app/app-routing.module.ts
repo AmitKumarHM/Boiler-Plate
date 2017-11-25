d36b302f056b0105e096582a365b4e4970e32658
jaydeepresources <jaydeepresources@github.com>
2017-11-25 15:07:00 +0530

    Everything under one app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
+import { DetailComponent } from './detail/detail.component';
+import { PipesComponent } from './pipes/pipes.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'template-form', component: TemplateFormComponent },
+  { path: 'reactive-form', component: ReactiveFormComponent },
+  { path: 'item/:id', component: DetailComponent },
+  { path: 'item', component: ItemsComponent },
+  { path: 'pipes', component: PipesComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ItemsComponent } from './items/items.component';
+import { DetailComponent } from './detail/detail.component';
+import { ChildComponent } from './child/child.component';
+import { ColorDirective } from './color.directive';
+import { PipesComponent } from './pipes/pipes.component';
+import { ReversePipe } from './reverse.pipe';
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import lcoaleDECH from '@angular/common/locales/de';
+registerLocaleData(lcoaleDECH);
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    NavbarComponent,
+    HomeComponent,
+    ReactiveFormComponent,
+    ItemsComponent,
+    DetailComponent,
+    ChildComponent,
+    ColorDirective,
+    PipesComponent,
+    ReversePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: "de" }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
